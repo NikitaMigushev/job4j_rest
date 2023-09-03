0431578a0a1a66b9841363b3d63557fbd1159b09
Nikita Migushev <migushev@gmail.com>
2023-09-03 11:43:21 +0300

    1. RESTFul. Описание архитектуры [#6884 #441452]

--- db/scripts/001_ddl_create_user_table.sql
+++ db/scripts/001_ddl_create_user_table.sql
@@ -1,6 +1,6 @@
 create table app_user (
     id bigserial primary key,
-    username varchar(2000) not null,
+    username varchar(2000) not null unique,
     password varchar(2000) not null,
     enabled boolean not null
 );
